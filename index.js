a838f00e55290d029c7f8b4f089d5fbfa5199dc6
takamoso <htmlcssjslove@gmail.com>
2024-05-28 00:59:56 +0900

    Add comments

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import {onRequest} from 'firebase-functions/v2/https'
 import puppeteer from 'puppeteer-chromium-resolver'
 
-export const scrape = onRequest({memory: '2GiB'}, async (req, res) => {
+export const scrape = onRequest({
+  // メモリは最低でも1GB推奨だがギリギリだと処理が中断することがあるため余裕を持たせておく
+  memory: '2GiB',
+}, async (req, res) => {
+  // ローカルにインストールされたChromiumを解決する
   const stats = await puppeteer()
+  // ブラウザを起動する
   const browser = await stats.puppeteer.launch({
+    // Chromiumの実行パス
     executablePath: stats.executablePath,
     args: [
       '--disable-gpu',
@@ -17,6 +23,7 @@ export const scrape = onRequest({memory: '2GiB'}, async (req, res) => {
     // 古いヘッドレスモード（パフォーマンスがいい）
     headless: 'shell',
   })
+  // 新規ページを開く
   const page = await browser.newPage()
 
   // ページにアクセスする
